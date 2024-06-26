48ac6ad81e01ae01aec95e0da13da2555aa0bf98
Shukaaa <asappokimane@gmail.com>
2024-06-27 01:19:43 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		frame: false,
+		transparent: true,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			contextIsolation: true,
+			enableRemoteModule: false, // Ensure this is set to false
+		},
+		icon: path.join(__dirname, 'assets', 'converta-light.png'),
+	});
+
+	mainWindow.loadFile(path.join(__dirname, 'index.html')).catch((err) => {
+		console.error('Failed to load index.html:', err);
+	});
+
+	mainWindow.webContents.openDevTools();
+	mainWindow.setResizable(true);
+	mainWindow.setMenuBarVisibility(false);
+	mainWindow.on('closed', () => {
+		mainWindow.destroy();
+	});
+
+	ipcMain.on('minimize-window', () => {
+		mainWindow.minimize();
+	});
+
+	ipcMain.on('maximize-window', () => {
+		if (mainWindow.isMaximized()) {
+			mainWindow.unmaximize();
+		} else {
+			mainWindow.maximize();
+		}
+	});
+
+	ipcMain.on('close-window', () => {
+		mainWindow.close();
+	});
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
